c82a2aa7fc3eb9fb399ee85be97618f477f5ec1c
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-09-11 22:51:07 +0200

    Add login service

--- TrainingPlanner/src/app/app.module.ts
+++ TrainingPlanner/src/app/app.module.ts
@@ -1,26 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { JwtHelperService, JwtModule } from "@auth0/angular-jwt";
+import { AccountModule } from "./account/account.module";
+import { SharedModule } from "./shared/shared.module";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NavbarComponent } from './navbar/navbar.component';
+export function getToken(): string {
+  return localStorage.getItem("token");
+}
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavbarComponent,
-      NavbarComponent,
-      NavbarComponent
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, NavbarComponent],
+  imports: [
+    BrowserModule,
+    AccountModule,
+    SharedModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: getToken
+      }
+    })
+  ],
+  providers: [JwtHelperService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ TrainingPlanner/src/app/services/Login.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { RegisterData } from "../models/RegisterData";
+import { LoginData } from "../models/LoginData";
+
+@Injectable({
+  providedIn: "root"
+})
+export class LoginService {
+  constructor(
+    private jwtHelper: JwtHelperService,
+    private client: HttpClient
+  ) {}
+
+  isUserAuthenticated() {
+    const token = localStorage.getItem("jwt");
+    if (token && !this.jwtHelper.isTokenExpired(token)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  register(registerData: RegisterData) {
+    return this.client.post(
+      "https://localhost:44383/api/Account/Register",
+      registerData,
+      {
+        headers: new HttpHeaders({
+          "Content-Type": "application/json"
+        })
+      }
+    );
+  }
+
+  login(loginData: LoginData) {
+    return this.client.post(
+      "https://localhost:44383/api/Account/Login",
+      loginData,
+      {
+        headers: new HttpHeaders({
+          "Content-Type": "application/json"
+        })
+      }
+    );
+  }
+}
