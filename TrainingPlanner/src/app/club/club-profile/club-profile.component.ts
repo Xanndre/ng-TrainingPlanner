a90f9ea46f0135e391e311ea86da1842a5cf46fe
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-10-17 12:16:49 +0200

    Change club-add to club-profile component

---
+++ TrainingPlanner/src/app/club/club-profile/club-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-club-profile',
+  templateUrl: './club-profile.component.html',
+  styleUrls: ['./club-profile.component.css']
+})
+export class ClubProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- TrainingPlanner/src/app/club/club.module.ts
+++ TrainingPlanner/src/app/club/club.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ClubAddComponent } from './club-add/club-add.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from '../guards/AuthGuard.service';
+import { ClubProfileComponent } from './club-profile/club-profile.component';
 
 const routes: Routes = [
   {
     path: 'profile/clubs',
-    component: ClubAddComponent,
+    component: ClubProfileComponent,
     canActivate: [AuthGuardService]
   }
 ];
 
 @NgModule({
-  declarations: [ClubAddComponent],
+  declarations: [ClubProfileComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
-  exports: [ClubAddComponent]
+  exports: [ClubProfileComponent]
 })
 export class ClubModule {}
