b5d3ac9860efbbde869aace62387ac6ee8b91205
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-10-28 21:24:58 +0100

    Fix phone number validation

--- TrainingPlanner/src/app/validators/PatternValidator.ts
+++ TrainingPlanner/src/app/validators/PatternValidator.ts
@@ -1,7 +1,10 @@
 import { AbstractControl } from '@angular/forms';
 
 export function isPhoneNumber(control: AbstractControl) {
-  if (!/[0-9]{3}-[0-9]{3}-[0-9]{3}/.test(control.value)) {
+  if (
+    !/[0-9]{3}-[0-9]{3}-[0-9]{3}/.test(control.value) ||
+    control.value.length !== 11
+  ) {
     return { isPhoneNumber: true };
   }
 }
