8969111dedc0ea9cf7880ce46970597b2e8c73ee
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-11-06 14:21:52 +0100

    Add change password errors

--- TrainingPlanner/src/app/account/change-password/change-password.component.ts
+++ TrainingPlanner/src/app/account/change-password/change-password.component.ts
@@ -5,6 +5,8 @@ import { FormBuilder } from '@angular/forms';
 import { ChangePassword } from 'src/app/models/User/ChangePassword';
 import { ChangePasswordControls } from './change-password-controls';
 import { ChangePasswordForm } from './change-password-form';
+import { MatDialog } from '@angular/material';
+import { ErrorDialogComponent } from 'src/app/shared/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-change-password',
@@ -20,7 +22,8 @@ export class ChangePasswordComponent implements OnInit {
   constructor(
     private loginService: LoginService,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit() {
@@ -36,8 +39,26 @@ export class ChangePasswordComponent implements OnInit {
       currentPassword: this.passwordForm.passwordForm.value.currentPassword,
       newPassword: this.passwordForm.passwordForm.value.password
     };
-    this.loginService.changePassword(this.object).subscribe(() => {
-      this.router.navigate(['/profile/user']);
+    this.loginService.changePassword(this.object).subscribe(
+      () => {
+        this.router.navigate(['/profile/user']);
+      },
+      err => {
+        if (err.error === `There's no user with such email`) {
+          this.showError(`There's no user with such email.`);
+        } else if (err.error === `Provided email address doesn't match yours`) {
+          this.showError(`Provided email address doesn't match yours.`);
+        } else {
+          this.showError('Invalid change password attempt.');
+        }
+      }
+    );
+  }
+
+  showError(error: string): void {
+    this.dialog.open(ErrorDialogComponent, {
+      data: { errorMsg: error },
+      width: '400px'
     });
   }
 }
