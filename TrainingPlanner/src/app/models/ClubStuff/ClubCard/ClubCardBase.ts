ad193b022f52d64d0b7bf5ae4e7a37c40b85f349
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-11-08 14:36:07 +0100

    Add user to card

--- TrainingPlanner/src/app/models/ClubStuff/ClubCard/ClubCardBase.ts
+++ TrainingPlanner/src/app/models/ClubStuff/ClubCard/ClubCardBase.ts
@@ -1,6 +1,9 @@
+import { User } from '../../User/User';
+
 export class ClubCardBase {
   id: number;
   clubName: string;
   name: string;
   expirationDate: Date;
+  user: User;
 }
--- TrainingPlanner/src/app/models/TrainerStuff/TrainerCard/TrainerCardBase.ts
+++ TrainingPlanner/src/app/models/TrainerStuff/TrainerCard/TrainerCardBase.ts
@@ -1,6 +1,9 @@
+import { User } from '../../User/User';
+
 export class TrainerCardBase {
   id: number;
   trainerName: string;
   name: string;
   expirationDate: Date;
+  user: User;
 }
